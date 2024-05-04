ec5d4e1add9a1f773ea232c4483172d3b54b001f
Guille1058 <150956074+Guille1058@users.noreply.github.com>
2024-05-04 21:59:49 +0000

    Update documento.java
    Adición de las funciones (realizarSuma), (realizarResta) y (realizarDivision) junto con sus respectivas llamadas.

--- documento.java
+++ documento.java
@@ -4,6 +4,20 @@ public class documento {
         int resultado = a * b;
         return resultado;
     }
+    public static int realizarSuma(int a, int b) {
+        int resultado = a + b;
+        return resultado;
+    }
+    public static int realizarResta(int a, int b) {
+        int resultado = a - b;
+        return resultado;
+    }
+
+    public static int realizarDivision(int a, int b) {
+        int resultado = a / b;
+        return resultado;
+    }
+    
     public static void main(String[] args) {
         Scanner leer = new Scanner(System.in);
         System.out.println("Introduce el primer número a continuación");
@@ -12,5 +26,8 @@ public class documento {
         int b = leer.nextInt();
         leer.close();
         System.out.print("El resultado de a * b es: "+multiplicar(a, b));
+        System.out.print("El resultado de a + b es: "+sumar(a, b));
+        System.out.print("El resultado de a - b es: "+resta(a, b));
+        System.out.print("El resultado de a / b es: "+division(a, b));
     }
 }
